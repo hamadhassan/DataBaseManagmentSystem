37fe87aeb81fab00511311ef2af9518aa6e195a4
Hammad Hassan <mhhjutt@gmail.com>
2023-02-28 19:22:03 +0500

    Lab-07 Completed

---
+++ Lab-07/CrystalReport-B-Lab-07/Form1.cs
@@ -0,0 +1,40 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrystalReport_B_Lab_07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowReport();
+        }
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            ShowReport();
+        }
+        private void ShowReport()
+        {
+            ReportDocument r = new ReportDocument();
+            string path = Application.StartupPath;
+            string reportpath = @"CrystalReport2.rpt";
+            string fpath = Path.Combine(path, reportpath);
+            r.Load(fpath);
+            crystalReportViewer1.ReportSource = r;
+        }
+    }
+}
