90c964e9cb181bd9fdf98cbc439c9582ed99a5fd
Hammad Hassan <mhhjutt@gmail.com>
2023-04-03 11:36:25 +0500

    Learn ASP.NET CORE

---
+++ ASP.NET CORE/BloggieMVC/Bloggie/Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,40 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BloggiedbContext bloggiedbContext;
+
+        public AdminTagsController(BloggiedbContext bloggiedbContext) 
+        {
+            this.bloggiedbContext = bloggiedbContext;
+        }
+
+        /// <summary>
+        /// Actions
+        /// </summary>
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+            bloggiedbContext.Tags.Add(tag);
+            bloggiedbContext.SaveChanges();
+            return View("Add");
+        }
+    }
+}
---
+++ ASP.NET CORE/BloggieMVC/Bloggie/Bloggie.Web/Data/BloggiedbContext.cs
@@ -0,0 +1,18 @@
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggiedbContext : DbContext
+    {
+        public BloggiedbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        /// <summary>
+        /// The dbSet will create class in the Database using the entity framework
+        /// </summary>
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
